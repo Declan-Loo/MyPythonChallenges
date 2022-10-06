3ffbda5daf2fc92dbf2aee56f0f87bdd9d989828
Declan Loo <41332340+mightywern@users.noreply.github.com>
2022-10-06 10:18:34 +0800

    Create TU14.py

---
+++ StringSlicing/TU14.py
@@ -0,0 +1,14 @@
+#Initialises variable
+MyString = "apple pie"
+
+#Skips one letter of the variable 
+print(MyString[0:len(MyString):2])
+
+#Prints the variable backwards
+print(MyString[::-1])
+
+#Prints until the 2nd character of the string
+print(MyString[:2])
+
+#Skips 2 letters of the variable
+print(MyString[0:len(MyString):3])
