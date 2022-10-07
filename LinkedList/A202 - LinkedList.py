fb3dac6d0385aae7ac1d7ef510abd1451797aab7
Declan Loo <41332340+mightywern@users.noreply.github.com>
2022-10-07 12:38:08 +0800

    Update A202 - LinkedList.py

--- LinkedList/A202 - LinkedList.py
+++ LinkedList/A202 - LinkedList.py
@@ -9,4 +9,19 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-first = Node(1)
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert(self, prev_node, new_data):
+        if prev_node is None:
+            return "The given previous node must be in the Linked List."
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+
+first = Node()
