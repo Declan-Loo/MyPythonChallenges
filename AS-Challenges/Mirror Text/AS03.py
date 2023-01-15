67c2e9ab90fd1f935e0c5af22171fa090b1d31c7
Declan Loo <188declan@gmail.com>
2023-01-15 18:16:23 +0800

    Create AS03.py

---
+++ AS-Challenges/Mirror Text/AS03.py
@@ -0,0 +1,10 @@
+"""
+AS03	
+50	
+MIRROR TEXT	
+A jokes shop company wants people to enter in text, but when they display it they want it to be spelt backwards.	
+"""
+
+text = input("Enter text: ")
+print()
+print(text[::-1])
