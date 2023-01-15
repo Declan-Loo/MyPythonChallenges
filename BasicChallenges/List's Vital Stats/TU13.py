5c00813667b8a53de76aad1dfb003cff4480de73
Declan Loo <188declan@gmail.com>
2023-01-15 18:42:32 +0800

    Create TU13.py

---
+++ BasicChallenges/List's Vital Stats/TU13.py
@@ -0,0 +1,22 @@
+"""
+TU13	
+15	
+LIST'S VITAL STATS	
+Learn how to find the Len, min, max and average of a list
+"""
+
+MyList = [2,4,8,16,32,64]
+#Print total of elements in list
+print(sum(MyList))
+
+#Min element
+print(min(MyList))
+
+#Max element
+print(max(MyList))
+
+#Number of elements
+print(len(MyList))
+
+#Average
+print((sum(MyList))/(len(MyList)))
