9efe4a12419ee7af51dd930e2005360800a400bc
Declan Loo <188declan@gmail.com>
2023-01-15 18:39:08 +0800

    Create TU05.py

---
+++ BasicChallenges/IF Statements/TU05.py
@@ -0,0 +1,12 @@
+"""
+TU05	
+12	
+IF STATEMENTS	
+Handling simple IF Statements
+"""
+
+test = input("Is it raining? y/n ")
+if test in ['y','Y','yes','Yes']:
+  print("Oh dear, no football today!")
+else:
+  print("Great we can play!")
