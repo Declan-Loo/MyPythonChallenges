12470458c0de5e7c459755ce375fef06cf4806a9
Declan Loo <188declan@gmail.com>
2023-01-15 18:37:25 +0800

    Create TU02.py

---
+++ BasicChallenges/Input & Variables/TU02.py
@@ -0,0 +1,14 @@
+"""
+TU02	
+10	
+INPUT & VARIABLES	
+Learn what a variable is and how to use it
+"""
+
+"""
+MyName = input('Please enter your name: ')
+print('Hello',MyName)
+"""
+
+name = input("Name? ")
+print("Hey",name +"!")
