9791058023ec5b168d48d1e6dd24da2bd6219ad2
Declan Loo <188declan@gmail.com>
2023-01-15 18:38:23 +0800

    Create TU04.py

---
+++ BasicChallenges/Variable Number Machine/TU04.py
@@ -0,0 +1,26 @@
+"""
+TU04	
+10	
+VARIABLE NUMBER MACHINE	
+input, print, variables
+"""
+
+"""
+EXAMPLE:
+TaylorSwiftBorn = 1989
+MyAge = int(input('When were you born?'))
+AgeDifference = MyAge - TaylorSwiftBorn
+print("Taylor Swift is",AgeDifference,"years older than you")
+"""
+#Exercise 2
+RihannaBorn = 1988
+MyAge = int(input('When were you born?'))
+AgeDifference = MyAge - RihannaBorn
+print("Rihanna is",AgeDifference,"years older than you")
+
+#Exercise 3
+CelebrityName = input("Name of celebrity? ")
+CelebrityBorn = int(input("When was "+CelebrityName+" born? "))
+MyAge = int(input('When were you born?'))
+AgeDifference = MyAge - CelebrityBorn
+print(CelebrityName, "is",AgeDifference,"years older than you")
