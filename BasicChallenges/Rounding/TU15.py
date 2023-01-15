913da5832afe98fa695d4d96a844e765a647b6b3
Declan Loo <188declan@gmail.com>
2023-01-15 18:41:48 +0800

    Create TU15.py

---
+++ BasicChallenges/Rounding/TU15.py
@@ -0,0 +1,15 @@
+"""
+TU15	
+15	
+ROUNDING	
+Learn how to round numbers in Python.
+"""
+
+x=3.33333333
+print(x)
+print(round(x,3))
+print(x)
+
+mynumbers = [1.5,2.5,3.5,4.5,5,5,6.5,7.5]
+for i in mynumbers:
+    print(round(i))
