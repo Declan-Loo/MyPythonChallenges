e56133ca4f27021a770d44798b4dd5673fc1af78
Declan Loo <188declan@gmail.com>
2023-01-15 18:48:38 +0800

    Create TU16.py

---
+++ BasicChallenges/F Strings (Python v3.6 and above)/TU16.py
@@ -0,0 +1,14 @@
+"""
+TU16	
+25	
+F STRINGS	
+Learn how to format variables and lists into tables. New in Python 3.6
+"""
+
+name = "Eric"
+age = 74
+print("Hello",name,"You are",age)
+print(f"Hello, {name}. You are {age}.")
+
+Ringgit = 129
+print(f"RM{Ringgit}")
