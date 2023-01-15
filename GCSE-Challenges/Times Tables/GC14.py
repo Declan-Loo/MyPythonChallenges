1237a50e3bfd20772d2c098f0cd869231e823ece
Declan Loo <188declan@gmail.com>
2023-01-15 18:31:52 +0800

    Create GC14.py

---
+++ GCSE-Challenges/Times Tables/GC14.py
@@ -0,0 +1,12 @@
+"""
+GC14	
+25	
+TIMES TABLES	
+Primary teachers would love to have a Times Tables generator to copy and paste.
+"""
+
+for i in range(1,13):
+  print(i,"TIMES TABLE")
+  for j in range(1,13):
+    print(i,"x",j,"=",i*j)
+  print("\n")
